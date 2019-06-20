58e99e10b71f4b7c81e97134d66fdd345dc3ae1a
Grant Orndorff <grant@amida.com>
2019-06-20 11:06:21 -0400

    adding material-ui and react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, NavLink, Redirect, Switch } from "react-router-dom";
+
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+import deepOrange from '@material-ui/core/colors/deepOrange';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+
 import './App.css';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: deepOrange,
+  },
+});
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <AppBar position="static">
+          <Toolbar>
+            <NavLink to="/upload">
+              <Button color="inherit">
+                Upload HL7
+              </Button>
+            </NavLink>
+            <NavLink to="/explore">
+              <Button color="inherit">
+                Explore HL7
+              </Button>
+            </NavLink>
+          </Toolbar>
+        </AppBar>
+        <Switch>
+          <Route path="/upload">
+            Uploading
+          </Route>
+          <Route path="/explore">
+            Exploring
+          </Route>
+          <Redirect to="/explore" />
+        </Switch>
+      </ThemeProvider>
+    </Router>
   );
 }
 
