8e8120d0f5850b94bbfea4f40ef9c1f7138ddd2d
Grant Orndorff <grant@amida.com>
2019-07-18 13:49:23 -0400

    parsed message view mocked

--- src/views/MessagePage.tsx
+++ src/views/MessagePage.tsx
@@ -1,16 +1,24 @@
 import React, { useState } from 'react';
 import { RouteComponentProps } from 'react-router';
 
-import { Typography, makeStyles, TextField } from '@material-ui/core';
+import { Typography, makeStyles, TextField, List, ListSubheader, ListItem, ListItemText } from '@material-ui/core';
+
+import mockMessage from '../data/mockMessage';
 
 const useStyles = makeStyles(theme => ({
   container: {
     display: 'grid',
     height: '100vh',
-    gridTemplateColumns: '1fr 1fr 1fr 1fr',
-    gridTemplateRows: '1fr 8fr',
+    gridTemplateColumns: '1fr 2fr 1fr 1fr',
+    gridTemplateRows: 'auto 3fr 4fr',
+    // gridTemplateAreas: `
+    //   "title filter filter select"
+    //   "raw raw raw raw"
+    //   "list list list list"
+    // `,
     gridTemplateAreas: `
       "title filter filter select"
+      "raw raw list list"
       "raw raw list list"
     `,
   },
@@ -33,14 +41,35 @@ const useStyles = makeStyles(theme => ({
     gridArea: 'raw',
     fontFamily: 'monospace',
     overflow: 'scroll',
-    boxShadow: 'inset -10px 0 10px -10px #b0b0b0',
+    boxShadow: 'inset 0 10px 10px -10px #b0b0b0',
+    marginTop: theme.spacing(1),
     padding: theme.spacing(2, 1),
     fontSize: '1.2em',
     lineHeight: '1.3em',
     whiteSpace: 'nowrap',
+    // resize: 'vertical',
   },
   segmentListContainer: {
     gridArea: 'list',
+    overflow: 'scroll',
+    marginTop: theme.spacing(1),
+  },
+  listSection: {
+    backgroundColor: 'white',
+  },
+  listSectionList: {
+    backgroundColor: 'white',
+    padding: 0,
+  },
+  selectedListSectionList: {
+    backgroundColor: 'lightblue',
+    padding: 0,
+  },
+  listSectionListHeader: {
+    backgroundColor: '#f0f0f0',
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+    textOverflow: 'ellipsis',
   },
 
   selectedSegment: {
@@ -61,21 +90,7 @@ const useStyles = makeStyles(theme => ({
 const MessagePageImpl: React.FC<RouteComponentProps> = (props) => {
   const classes = useStyles();
   const fileName = 'Mock File';
-  const message = {
-    rawMessage: `
-MSH|^~\\&|App3|Facility2|App6|Facility5|201906191553||SIU^S12|2226282|D^R|2.3.1
-SCH|33910|33910||||RWMA-FOLLOWUP|Test Appointment|RWFU^RWMA-FOLLOWUP|15|minutes|^^^201505191300|||||username|||||||||BOOKED
-PID||7251|7251||NICOLAS^NOLA^^V||19750513|M||2054-5|136 ARMSTRONG DIVIDE^SUITE 027^RUPERTSTAD^WY^17138||568-892-0027|524-520-7726|eng^English|S||PATID7251^0^M10|353-672-2203|53850057^WY||2054-5^
-PD1||||A85832^SAUER^ALEXIS
-PV1||O|1000^2019^01||||371^WILKINSON^SABRINA|90648^HERMANN^JASPER|||||||||||16747007|||||||||||||||||||||||||201906191553||||||33910
-AIG|||providerusername|||||201505191300|||15|minutes
-AIL|||2^TEST DEPARTMENT|||201505191300|||15|minutes
-    `,
-    fileId: 'somelongstringofnumbers',
-    messageNumWithinFile: 0,
-    parsedMessage: {
-    },
-  };
+  const message = mockMessage;
   const [[selectedSegmentIndex, selectedFieldIndex], setSelected] = useState([-1, -1]);
   return (
     <div className={classes.container}>
@@ -97,14 +112,16 @@ AIL|||2^TEST DEPARTMENT|||201505191300|||15|minutes
         label="Message Number"
       />
       <div className={classes.rawMessageContainer}>
-        {message.rawMessage.split(/(\r\n)|(\r)|(\n)/).filter(segment => !!segment).map((segment, segmentIndex) => (
+        {message.rawMessage.split('\n').filter(segment => !!segment).map((segment, segmentIndex) => (
           <div
             className={selectedSegmentIndex === segmentIndex ? classes.selectedSegment : undefined}
+            key={`rawSegment-${segmentIndex}`}
           >
             {segment.split('|').map((field, fieldIndex) => (
               <span>
                 {fieldIndex !== 0 && '|'}
                 <span
+                  key={`rawField-${segmentIndex}-${fieldIndex}`}
                   className={selectedSegmentIndex === segmentIndex && selectedFieldIndex === fieldIndex ? classes.selectedField : classes.field}
                   onClick={() => setSelected([segmentIndex, fieldIndex])}
                 >
@@ -115,7 +132,41 @@ AIL|||2^TEST DEPARTMENT|||201505191300|||15|minutes
           </div>
         ))}
       </div>
-      <div className={classes.segmentListContainer}>
+      <div
+        className={classes.segmentListContainer}
+      >
+        <List
+          subheader={<li />}
+        >
+          {message.parsedMessage.map((segment, segmentIndex) => (
+            <li
+              className={classes.listSection}
+              key={`parsedSegment-${segmentIndex}`}
+            >
+              <ul
+                className={selectedSegmentIndex === segmentIndex ? classes.selectedListSectionList : classes.listSectionList}
+              >
+                <ListSubheader
+                  className={classes.listSectionListHeader}
+                >
+                  {segment.definition && segment.definition.description ? segment.definition.description : segment.name}
+                </ListSubheader>
+                {(segment.children as any[]).map((field, fieldIndex) => !field ? undefined : (
+                  <ListItem
+                    key={`parsedField-${segmentIndex}-${fieldIndex}`}
+                    className={selectedSegmentIndex === segmentIndex && selectedFieldIndex === fieldIndex ? classes.selectedField : classes.field}
+                    onClick={() => setSelected([segmentIndex, fieldIndex])}
+                    >
+                    <ListItemText
+                      primary={field.value ? field.value : '(empty)'}
+                      secondary={field.definition && field.definition.description ? field.definition.description : field.name}
+                    />
+                  </ListItem>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </List>
       </div>
     </div>
   );
